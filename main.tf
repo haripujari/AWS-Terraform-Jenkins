2edd5e0005483f652e43e6d41e4faaad16e447e3
haripujari <92815890+haripujari@users.noreply.github.com>
2025-07-03 19:20:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"  # Change to your preferred region
 }
 
-resource "aws_s3_bucket" "my_bucket1" {
+resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name-12345"  # Change this to a globally unique name
   
 
